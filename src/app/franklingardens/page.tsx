61e009fab9f8ba8987dbf164331f33cb50b4cdb7
luwai <luwaiwong@gmail.com>
2025-11-18 10:33:00 -0500

    chore: add franklingardens

---
+++ src/app/franklingardens/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import BehavioursDashboard from "@/components/behavioursDashboard/BehavioursDashboard";
+
+export default function FranklinGardens() {
+  return (
+    <BehavioursDashboard
+      name="franklingardens"
+      title="Franklin Gardens Behaviours Dashboard"
+      goal={15}
+    />
+  );
+}
