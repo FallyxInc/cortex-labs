70bbbba83692cc7890b74982fe53e0171084887b
luwai <luwaiwong@gmail.com>
2025-11-26 09:31:07 -0500

    fix: read correct env

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,11 +4,11 @@ import { getDatabase } from 'firebase-admin/database';
 // Initialize Firebase Admin if not already initialized
 if (!getApps().length) {
   const serviceAccount = {
-    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID || '',
-    privateKeyId: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID || '',
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
-    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL || '',
-    clientId: process.env.FIREBASE_ADMIN_CLIENT_ID || '',
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID || '',
+    privateKeyId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY_ID || '',
+    privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+    clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL || '',
+    clientId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_ID || '',
   };
   
   initializeApp({
